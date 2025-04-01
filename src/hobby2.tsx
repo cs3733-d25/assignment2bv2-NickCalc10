34a128daadbb0856f126c3ab65b7f6d29ec9ae4e
LoganWin <logan9win@gmail.com>
2025-04-01 17:42:19 -0400

    My hobby 2 page basics

---
+++ src/hobby2.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+interface hobbyProps {
+    hobbyName: string
+    description: string
+    imageUrl: string
+}
+
+const hobby2: React.FC<hobbyProps> = ({hobbyName, description, imageUrl}) => {
+    return (
+        <div>
+            <h1>{hobbyName}</h1>
+            <p>{description}</p>
+            <img src={imageUrl} width="300" height = "300"
+            />
+        </div>
+    )
+}
+
+export default hobby2;
